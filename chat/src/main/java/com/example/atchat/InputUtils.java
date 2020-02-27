20d9f4ee3d9ba60fd57d21f9cdf818a1b4443661
Agata Tafelska <agata.tafelska95@gmail.com>
2020-02-27 14:23:42 +0100

    Add loading and input error checking in MainActivity

---
+++ chat/src/main/java/com/example/atchat/InputUtils.java
@@ -0,0 +1,22 @@
+package com.example.atchat;
+
+import java.util.regex.Pattern;
+
+public class InputUtils {
+    public static boolean isValidHost(String host) {
+        final String zeroTo255 = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
+        final String IP_REGEX = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+        final Pattern IP_PATTERN = Pattern.compile(IP_REGEX);
+        return IP_PATTERN.matcher(host).matches() || host.equals("localhost");
+    }
+
+    public static boolean isValidUserName(String username) {
+        final String USERNAME_REGEX = "[a-zA-Z0-9\\._\\-]{3,20}";
+        final Pattern USERNAME_PATTERN = Pattern.compile(USERNAME_REGEX);
+        return USERNAME_PATTERN.matcher(username).matches();
+    }
+
+    public static boolean isMessageValid(String text) {
+        return !text.isEmpty();
+    }
+}
--- chat/src/main/java/com/example/atchat/MainActivity.java
+++ chat/src/main/java/com/example/atchat/MainActivity.java
@@ -6,6 +6,9 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextHost;
     private EditText editTextUsername;
     private Button buttonJoinChat;
+    private ProgressBar progressBar;
+    private TextView textViewErrorMessage;
+
     private ActivitiesCoordinator coordinator;
 
     @Override
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         editTextHost = findViewById(R.id.host);
         editTextUsername = findViewById(R.id.username);
         buttonJoinChat = findViewById(R.id.join_button);
+        progressBar = findViewById(R.id.logging_progress_bar);
+        textViewErrorMessage = findViewById(R.id.error_message);
 
         editTextUsername.addTextChangedListener(loginTextWatcher);
         editTextHost.addTextChangedListener(loginTextWatcher);
@@ -37,10 +45,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String host = editTextHost.getText().toString();
                 String username = editTextUsername.getText().toString();
-                coordinator.joinChat(host, username, MainActivity.this);
+
+                if (!InputUtils.isValidHost(host)) {
+                    Toast.makeText(MainActivity.this, getApplicationContext().getResources().getString(R.string.incorrect_host_toast_message), Toast.LENGTH_LONG).show();
+                }
+                else if (!InputUtils.isValidUserName(username)) {
+                    Toast.makeText(MainActivity.this, getApplicationContext().getResources().getString(R.string.incorrect_username_toast_message), Toast.LENGTH_LONG).show();
+                } else {
+                    showLoading();
+                    coordinator.joinChat(host, username, MainActivity.this);
+                }
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        editTextHost.setVisibility(View.VISIBLE);
+        editTextUsername.setVisibility(View.VISIBLE);
+        buttonJoinChat.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        textViewErrorMessage.setVisibility(View.INVISIBLE);
     }
 
     private TextWatcher loginTextWatcher = new TextWatcher() {
@@ -62,5 +88,11 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void showLoading() {
+        progressBar.setVisibility(View.VISIBLE);
+        editTextHost.setVisibility(View.INVISIBLE);
+        editTextUsername.setVisibility(View.INVISIBLE);
+        buttonJoinChat.setVisibility(View.INVISIBLE);
+    }
 
 }
