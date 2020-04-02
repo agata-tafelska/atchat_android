27259f653b3ca1ca4a66e6128f3e46566a3388e7
Agata Tafelska <agata.tafelska95@gmail.com>
2020-04-02 16:59:01 +0200

    Add ability to go from MainActivity to RegisterActivity

--- chat/src/main/java/com/example/atchat/MainActivity.java
+++ chat/src/main/java/com/example/atchat/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button signInButton;
     private Button joinAsGuestButton;
+    private TextView textViewCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         signInButton = findViewById(R.id.sign_in_button);
         joinAsGuestButton = findViewById(R.id.join_as_guest_button);
+        textViewCreateAccount = findViewById(R.id.create_account_link);
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent intentToStartJoinAsGuestActivity =
                         new Intent(MainActivity.this, JoinAsGuestActivity.class);
                 startActivity(intentToStartJoinAsGuestActivity);
+            }
+        });
+
+        textViewCreateAccount.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
